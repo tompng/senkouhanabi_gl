d40cf39cc90b9e2aead24580916d2aba316a9fca
tompng <tomoyapenguin@gmail.com>
2020-09-02 01:12:32 +0900

    spark animation

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,9 @@ const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 100)
 const curves = new CurveManager(scene)
 
-for (let i = 0; i < 20; i++) {
-  spark({ x: 0, y: 0, z: 0 }, sphereRandom(), 0)
-}
+// for (let i = 0; i < 20; i++) {
+//   spark({ x: 0, y: 0, z: 0 }, sphereRandom(), 0)
+// }
 camera.up = new THREE.Vector3(0, 0, 1)
 document.body.appendChild(renderer.domElement)
 
@@ -30,13 +30,43 @@ targetRenderScene.add(targetRenderMesh)
 
 type P3 = { x: number; y: number; z: number }
 
-function spark(p: P3, v: P3, step: number) {
-  const life = 0.2 + Math.random()
+type SparkElement = {
+  p: P3
+  v: P3
+  life: number
+  at: number
+  w: number
+}
+const gravity = 20.4
+function nextSpark({ p, v, life, at, w }: SparkElement, time: number, out: SparkElement[]) {
+  const t = Math.min(time, at)
+  const p2 = { x: p.x + v.x * t, y: p.y + v.y * t, z: p.z + v.z * t - gravity * t * t / 2 }
+  const v2 = { ...v, z: v.z - gravity * t }
+  sparkCurve(p, v, p2, v2, life, w, t)
+  if (t === life) return
+  if (t === time) {
+    sparkCurve(p, v, p2, v2, life, w, t)
+    out.push({ p: p2, v: v2, life: life - t, at: at - t, w })
+    return
+  }
+  if (w < 1/16) return
+  for (let i = 0; i < 10; i++) {
+    const v3 = sphereRandom()
+    const vr = 10 * w
+    const life2 = life - t
+    const at2 = Math.min(life2, 0.02 + 2 * life2 * Math.random())
+    nextSpark({
+      p: p2,
+      v: { x: v2.x / 2 + vr * v3.x, y: v2.y / 2 + vr * v3.y, z: v2.z / 2 + vr * v3.z },
+      life: life2,
+      at: at2,
+      w: w / 2
+    }, time - t, out)
+  }
+}
+
+function sparkCurve(p: P3, v: P3, p2: P3, v2: P3, life: number, w: number, t: number) {
   const curve = curves.get()
-  const t = life
-  const g = 0.4
-  const p2 = { x: p.x + v.x * t, y: p.y + v.y * t, z: p.z + v.z * t - g * t * t / 2 }
-  const v2 = { ...v, z: v.z - g * t }
   curve.pa.x = p.x
   curve.pa.y = p.y
   curve.pa.z = p.z
@@ -49,20 +79,33 @@ function spark(p: P3, v: P3, step: number) {
   curve.db.x = v2.x * t
   curve.db.y = v2.y * t
   curve.db.z = v2.z * t
-  const c = 1 / (1 + step) * t / 2
-  curve.cola.setRGB(0.8 * c / 40, 0.4 * c / 40, 0.2 * c / 40)
+  const c = w * t * 4
+  curve.cola.setRGB(0.8 * c, 0.4 * c, 0.2 * c)
   curve.colb.setRGB(0.8 * c, 0.4 * c, 0.2 * c)
-  const r = 1 / (1 + step)
-  if (step == 2) return
-  for (let i = 0; i < 8; i++) {
-    const v3 = sphereRandom()
-    spark(p2, { x: v2.x / 2 + r * v3.x, y: v2.y / 2 + r * v3.y, z: v2.z / 2 + r * v3.z }, step + 1)
-  }
 }
 
+let sparks: SparkElement[] = []
+let twas = performance.now() / 1000
+
+curves.reset()
+const v = sphereRandom()
+const vr = 4
+sparks.push({ p: { x: 0, y: 0, z: 0 }, v: { x: vr * v.x, y: vr * v.y, z: vr * v.z }, life: 0.2, at: 0.2 * Math.random(), w: 1 })
+sparks.forEach(sp => nextSpark(sp, 1000, []))
 
 function animate() {
   const time = performance.now() / 1000
+  const dt = (time - twas) * 2
+  twas = time
+  curves.reset()
+  if (Math.random() < 0.2) {
+    const v = sphereRandom()
+    const vr = 4
+    sparks.push({ p: { x: 0, y: 0, z: 0 }, v: { x: vr * v.x, y: vr * v.y, z: vr * v.z }, life: 0.2, at: 0.2 * Math.random(), w: 1 })
+  }
+  const sparks2: SparkElement[] = []
+  sparks.forEach(sp => nextSpark(sp, dt, sparks2))
+  sparks = sparks2
   const th = time / 4 - Math.PI / 2
   camera.position.x = 3 * Math.cos(th)
   camera.position.y = 3 * Math.sin(th)
@@ -74,5 +117,6 @@ function animate() {
   renderer.setRenderTarget(null)
   renderer.render(targetRenderScene, targetRenderCamera)
   requestAnimationFrame(animate)
+  // setTimeout(animate, 100)
 }
 animate()
